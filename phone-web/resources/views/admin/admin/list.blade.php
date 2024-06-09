48bcb961fd6dcacacc9891ba1e26b1907844e3c4
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-06-09 17:49:24 +0700

    set up giao dien Admin

---
+++ phone-web/resources/views/admin/admin/list.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layouts.app')
+@section('style')
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('script')
+    <!-- <script src="{{url('assets/dist/js/pages/dashboard3.js')}}"></script> -->
+@endsection
--- phone-web/routes/web.php
+++ phone-web/routes/web.php
@@ -18,8 +18,12 @@ Route::get('admin', function () {
 });
 
 
-Route::get('admin/dashb', function () {
-    return view('admin.auth.login');
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
 });
 
 Route::get('/', function () {
